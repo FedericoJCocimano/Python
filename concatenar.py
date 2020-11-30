feb9fb903d8d2b8742ac2df799fa3a3a8bcbafdc
FedericoJCocimano <71039796+FedericoJCocimano@users.noreply.github.com>
2020-11-30 16:58:48 -0300

    Add files via upload

---
+++ concatenar.py
@@ -0,0 +1,6 @@
+nombre = "Federico"
+apellido = "Cocimano"
+print(nombre, apellido) #Esto no es concatenar, pero se obtiene el mismo resultado
+print(nombre + " " + apellido)
+print(f"{nombre} {apellido}")
+print("Hola me llamo: {} {}".format(nombre, apellido))
