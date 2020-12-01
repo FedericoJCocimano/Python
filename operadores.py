f059c565796ceea7302a89ca190a2bbe8c88f6c2
FedericoJCocimano <71039796+FedericoJCocimano@users.noreply.github.com>
2020-12-01 16:46:36 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,21 @@
+num1 = 15
+num2 = 20
+
+#Operadores aritmeticos
+resta = num2 - num1
+suma = num1 + num2
+multiplicacion = num1 * num2
+division = num2 / num1
+resto = num2 % num1
+
+#Operadores de asignacion
+num2 -= 5 #num2 = num2 - 5
+num1 += 5 #num1 = num1 + 5 
+num2 *= 2 #num2 = num2 * 2
+num1 /= 2 #num1 = num1 / 2
+num2 %= 2 #num2 = num2 % 2
+
+#Pre Incremento y Pre Decremento
+num1 = 1 + num1 #PI
+num2 = 1 - num2 #PD
+
