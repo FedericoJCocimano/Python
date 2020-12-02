77f6de3ae5404693b73b4820c8fa4374998bc750
FedericoJCocimano <71039796+FedericoJCocimano@users.noreply.github.com>
2020-12-02 17:36:20 -0300

    Add files via upload

---
+++ Ejercicios1.py
@@ -0,0 +1,21 @@
+'''
+Ejercicio 1:
+    -Ingresar variable "pais" y "Contintente".
+    -Mostrar su valor por pantalla.
+    -Indicar el tipo de dato por pantalla.
+'''
+def ejercicio1():
+    pais = str(input("Ingrese el pais: "))
+    continente = str(input("Ingrese el continente al que pertenece: "))
+    print(f"El pais es {pais} y se encuentra en el continente de {continente}")
+    dato = type(pais)
+    print("Tipo de dato: ", dato)
+
+'''
+Ejercicio 2: Escribir un script que nos muestre por pantalla
+todos los numeros pares del 1 al 120.
+'''
+
+def ejercicio2():
+    for x in range(1, 121):
+        continue
